a951154b24e073b573eadcedaf4a30e97dec2af2
miacc9576@gmail.com <miacc9576@gmail.com>
2025-10-23 22:44:12 +0200

    #

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,9 +1,9 @@
 // src/utils/api.ts
 
-export const API_BASE_URL = import.meta.env.VITE_API_URL;
+export const API_BASE_URL = import.meta.env._VITE_API_URL;
 
 console.log('API Base URL:', API_BASE_URL);
-console.log('API key:', import.meta.env.VITE_GOOGLE_MAPS_API_KEY)
+console.log('API key:', import.meta.env._VITE_GOOGLE_MAPS_API_KEY)
 
 /**
  * A wrapper around the native `fetch` function that automatically adds
